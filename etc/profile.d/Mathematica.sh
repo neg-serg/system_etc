47baa01bca268f38185f2da0162dc1f366c68553
Neg <serg.zorg@gmail.com>
2018-11-06 04:02:16 +0300

    update

---
+++ etc/profile.d/Mathematica.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+export MATHEMATICA_HOME="/usr/local/Wolfram/Mathematica/10.0"
---
+++ etc/profile.d/xdg.sh
@@ -0,0 +1,9 @@
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_DATA_HOME="${HOME}/.local/share"
+export XDG_DOWNLOAD_DIR="${HOME}/dw"
+export XDG_MUSIC_DIR="${HOME}/music"
+export XDG_DESKTOP_DIR="${HOME}/.local/desktop"
+export XDG_TEMPLATES_DIR=${XDG_DESKTOP_DIR}
+export XDG_DOCUMENTS_DIR="${HOME}/doc/"
+export XDG_PICTURES_DIR="${HOME}/pic"
+export XDG_VIDEOS_DIR="${HOME}/vid"
